cfbd2b46c16fc7127a173602c875d963864aa6c2
JB <zxsefv@gmail.com>
2023-01-16 15:47:23 +0900

    20230116_2

---
+++ 20230116_1.py
@@ -0,0 +1,13 @@
+# 1번
+# name = 'JB'
+# score = 100
+# print(f'{name}의 성적은 {score}이다.')
+'''''
+boxes = ['A', 'B', ['apple', 'banana', 'cherry']]
+print(len(boxes)) # 3
+print(boxes[2])  # ['apple', 'banana', 'cherry']
+print(boxes[2][-1]) #cherry
+print(boxes[-1][1][0]) #소문자 b
+'''''
+
+
---
+++ 20230116_DW.py
@@ -0,0 +1,12 @@
+print("It\'s SSAFY 9")
+
+a = 458345
+b = 623576
+print(a+b)
+
+greeting = 'Hello'
+month = ' July'
+print(greeting+month)
+
+hello = input()
+print(hello)
