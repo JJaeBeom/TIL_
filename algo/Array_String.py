26da8dd00604aec1d190b69467a1493b7223267c
JB <zxsefv@gmail.com>
2023-02-13 02:58:57 +0900

    20230212

--- algo/Array_String.py
+++ algo/Array_String.py
@@ -96,5 +96,80 @@ def binarySearch(a, N, key):
     return False #검색 실패. 없다는 뜻
     # 재귀로 이진 탐색
 ###################################################################
+# #선택정렬
+# def selectionSort(a, N):
+#     for i in range(N-1):
+#         minIdx = i
+#         for j in range(i+1, N):
+#             if a[minIdx] > a[j]:
+#                 minIdx = j
+#         a[i], a[minIdx] = a[minIdx], a[i]
+a = [7, 8, 9, 7, 6, 4, 5, 1, 3, 2]
+N = len(a)
+for i in range(N-1):
+    minIdx = i
+    for j in range(i+1, N):
+        if a[minIdx] > a[j]:
+            minIdx = j
+    a[i], a[minIdx] = a[minIdx], a[i]
+print(a)
+###################################################################
+#brute force
+# BruteForce
+def BruteForce(p, t):
+    i = 0 # t의 검색 인덱스(t에서의 비교 위치)
+    j = 0 # p의 검색 인덱스(p에서의 비교 위치)
+    # len(p) : 찾을 패턴의 길이
+    # len(t) : 전체 텍스트의 길이
+    while i < len(t) and j <len(p):
+        # 비교할 문장이 남아있고, 패턴을 찾기 전이면
+        if t[i] != p[j]: #서로 다른 글자를 만나면
+            i = i - j   #비교를 시작한 위치로
+            j = -1 # -1로 초기화(패턴의 시작 전으로)
+        i += 1 # 같건 다르건
+        j += 1 # i , j 하나씩 증가
+    if j == len(p): #패턴을 찾은 경우
+        return i - len(p) #위치 반환
+    else: #못찾은경우
+        return -1 # -1 반환
+    
+pattern = "Python"
+text = "Hello Python World"
+print(BruteForce(pattern, text))
+###############################################
+#BruteForce 부분합? 위에꺼랑 똑같이 나옴
+def BruteForce2(p, t):
+    for i in range(len(t)-len(p)+1):
+        for j in range(len(p)):
+            if t[i+j] != p[j]:
+                break
+        else:
+            return i
+        
+pattern = "Python"
+text = "Hello Python World"
+print(BruteForce2(pattern, text))
+############################################
+##pattern이 몇개가 있는지 세는 것.
+def BruteForce3(p, t):
+    cnt = 0
+    for i in range(len(t)-len(p)+1):
+        for j in range(len(p)):
+            if t[i+j] != p[j]:
+                break
+        else:
+            cnt += 1
+            #return i 중단하고 나가라고 했는데
+    return cnt
+        
+pattern = "Python"
+text = "Hello Python a Python World"
+print(BruteForce3(pattern, text))
+##################################################
+###################################################################
+###################################################################
+###################################################################
 #Gravity
 #Baby-gin
+#달팽이
+
