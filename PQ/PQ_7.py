d60d02c12f5157c3e037693b0c8533b11dcc2e18
정재범 <zxsefv@gmail.com>
2023-02-17 17:56:23 +0900

    20230217

---
+++ PQ/PQ_7.py
@@ -0,0 +1,16 @@
+def DFS(start):
+    visited[start] = 1
+    print(start, end= ' ')
+    for next in range(1, V+1):
+        if G[start][next] and not visited[next]:
+            DFS(next)
+
+V, E = map(int, input().split())
+data = list(map(int, input().split()))
+visited = [False] * (V+1)
+G = [[0]* (V+1) for _ in range(V+1)]
+for i in range(E):
+    n1, n2 = data[i*2], data[i*2+1]
+    G[n1][n2] = G[n2][n1] = 1
+
+DFS(1)
