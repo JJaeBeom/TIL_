82238a47e89ac41a0dd306be2dfbc5a362ccc268
정재범 <zxsefv@gmail.com>
2023-04-19 17:58:40 +0900

    20230419

---
+++ JS/day2.js
@@ -0,0 +1,27 @@
+// const nums = [1, 2, 3, 4]
+// nums.forEach(function (num){
+//     console.log(num ** 3)
+// })
+
+// const nums = [1,2,3,4,5,6,7,8]
+
+// for (let i = 0; i < nums.length; i++) {
+//   console.log('nums[' + i + ']: ' + nums[i])
+// }
+
+// const star = '*'
+// for (let i=1; i <= 5; i++){
+//     console.log(star)
+
+// }
+
+const star = '*'
+i = 1
+while (i < 6){
+    console.log(star)
+    star + '*'
+    i += 1
+}
+
+
+
